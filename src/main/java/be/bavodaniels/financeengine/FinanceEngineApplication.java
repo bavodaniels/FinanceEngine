27c2add0fd03777b8068a86ba104e8cd00fcbff5
bavodaniels <bavo.daniels@external.toyota-europe.com>
2022-04-30 21:35:55 +0200

    Empty project setup

---
+++ src/main/java/be/bavodaniels/financeengine/FinanceEngineApplication.java
@@ -0,0 +1,13 @@
+package be.bavodaniels.financeengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/bavodaniels/financeengine/FinanceEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package be.bavodaniels.financeengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
